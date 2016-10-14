310740c54b1bf3e105b103be3eff4a7571229270
Tarnov Sergey <starnov@marathonbet.ru>
2016-10-14 14:35:28 +0300

    start commits, make new change

---
+++ src/test/bitcoin-util-test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Copyright 2014 BitPay, Inc.
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+from __future__ import division,print_function,unicode_literals
+import os
+import bctest
+import buildenv
+import argparse
+
+help_text="""Test framework for bitcoin utils.
+
+Runs automatically during `make check`.
+
+Can also be run manually from the src directory by specifiying the source directory:
+
+test/bitcoin-util-test.py --src=[srcdir]
+"""
+
+
+if __name__ == '__main__':
+    try:
+        srcdir = os.environ["srcdir"]
+    except:
+        parser = argparse.ArgumentParser(description=help_text)
+        parser.add_argument('-s', '--srcdir')
+        args = parser.parse_args()
+        srcdir = args.srcdir
+    bctest.bctester(srcdir + "/test/data", "bitcoin-util-test.json", buildenv)
